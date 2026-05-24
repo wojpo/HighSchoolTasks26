8ddf85f6132ab823615a8b4a45fd0c10726416ce
goteusz-maszyk <mateusz.goszyk@gmail.com>
2026-05-24 15:18:18 +0200

    fix niech czemu działa

--- tasks/czemu-nie-dziala/src/index.js
+++ tasks/czemu-nie-dziala/src/index.js
@@ -3,29 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = 6969;
 
-app.options('/', async (req, res) => {
+app.get('/', async (req, res) => {
   const isFromCloudflare = req.headers['cf-ray'] || req.headers['cf-connecting-ip'] || req.headers['cf-visitor'];
   const hasUserAgent = req.headers['user-agent'];
-  const cors = !req.headers['origin']
 
   if (isFromCloudflare) {
-    res.status(407)
+    res.status(418)
   } else if (hasUserAgent) {
     res.status(406)
-    res.write("Your device has been blocked due to security rules. Please try again later.")
-  } else if (cors) {
-      res.status(418)
-      res.write("CORS are required")
+    res.write("Your browser has been blocked due to security rules. Please try again later.")
   } else {
-    return res.write('hack4KrakCTF{n0-jak-N13-DzI@lA-J@k-prz3ciez-dz1alA}');
+    res.write('hack4KrakCTF{n0-jak-N13-DzI@lA-J@k-prz3ciez-dz1alA}');
   }
 
   res.end();
-})
-
-app.get('/', async (req, res) => {
-  res.status(418)
-  res.end()
 })
 
 app.listen(PORT, (err) => {
